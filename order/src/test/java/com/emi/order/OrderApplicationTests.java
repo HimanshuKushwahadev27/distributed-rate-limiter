40303d8dec17a471d7e69606bea5050bb187a005
Eminence <himanshumaurya447756@gmail.com>
2026-04-09 16:40:25 +0000

    AC-4 init order

---
+++ order/src/test/java/com/emi/order/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emi.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
